37a3b6416856d88e0eb309b1ae9a1f0c6e8f23d8
Daniele <iximiel@gmail.com>
2020-03-24 17:02:27 +0000

    The C-like program for reading the i2Cbus works

--- src/i2c.cxx
+++ src/i2c.cxx
@@ -10,9 +10,36 @@ extern "C" {//remember ti include -li2c
 #include <errno.h>
 #include <string.h>
 
+#include <cmath>
+
+constexpr float Vin = 3.3f;
+constexpr float Vref = 3.3f;
 constexpr int adapterNumber = 1;
 constexpr int i2CAddress = 0x48;
-/// thi is the c style thing
+constexpr float K = 6.0f;//6mV C
+constexpr float zeroKelvin = 273.15f;
+constexpr float R1 = 15000.0f;
+constexpr float Rth0 =10000.0f;
+//parameters for Steinhart-Hart
+constexpr float sA = 0.00335402f;
+constexpr float sB = 0.000256985f;
+constexpr float sC = 2.62013e-6f;
+constexpr float sD = 6.38309e-8;
+//formula B
+constexpr float parB = 4100.0f;
+//lux sensor
+constexpr float Rlux0 = 70000.0f;
+constexpr float Pend = 0.7f;
+
+float VoltageFromADCVoltage(const long& res) {
+  return (res/256.0f) * Vref;
+}
+
+float ResinstanceFromADCVoltage(const long& res) {
+  float V = VoltageFromADCVoltage(res);
+  return  (R1*V) / (Vin - V);
+}
+// this is the c style thing
 int main(int, char**) {
   std::string device = "/dev/i2c-";
   device+=std::to_string(adapterNumber);
@@ -37,10 +64,12 @@ int main(int, char**) {
     long res = i2c_smbus_read_byte(file);
     res = i2c_smbus_read_byte(file);
     res = i2c_smbus_read_byte(file);
+    float Rlux = ResinstanceFromADCVoltage(res);
+    float Lux = pow(Rlux/Rlux0,1.0/-Pend);
     if(res <0 ){
       std::cerr << "Failed to read lux\n";
     } else {
-      std::cout << "Lux: " << res <<std::endl;
+      std::cout << "Lux: " << Lux <<std::endl;
     }
   }
   {
@@ -48,10 +77,21 @@ int main(int, char**) {
     long res = i2c_smbus_read_byte(file);
     res = i2c_smbus_read_byte(file);
     res = i2c_smbus_read_byte(file);
+    float V = VoltageFromADCVoltage(res);
+    float Rth = ResinstanceFromADCVoltage(res);
+    float lR = log(Rth/Rth0);
+    float lRsq = lR*lR;
+    float lRcb = lRsq*lR;
+    float T = 1.0f/(sA + sB*lR + sC*lRsq + sD*lRcb);
+    T = T-zeroKelvin - V*V / ( K*Rth);
+    lR = log(Rth/10000.0f);
+    float T2 = 1.0f/((1.0f/298.15f) + lR/parB);
+    T2 = T2-zeroKelvin - V*V / ( K*Rth);
     if(res <0 ){
       std::cerr << "Failed to read temperature\n";
     } else {
-      std::cout << "Temperature: " << res <<std::endl;
+      std::cout << "Temperature: " << T <<std::endl;
+      std::cout << "Temperature: " << T2 <<std::endl;
     }
   }
   return 0;
