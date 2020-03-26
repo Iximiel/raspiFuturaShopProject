696a4e89b480791ebe0f34b1a3ce16a3214e794d
Daniele <iximiel@gmail.com>
2020-03-26 10:18:44 +0000

    now also work, moe or less

--- srcTests/gpio_i2c.cxx
+++ srcTests/gpio_i2c.cxx
@@ -1,3 +1,5 @@
+/**Partially copied from the examples of libgpiod
+*/
 #include <iostream>
 #include <string>
 #include <gpiod.hpp>
@@ -10,14 +12,14 @@ constexpr unsigned int switchPIN = 17;
 constexpr unsigned int plusPIN   = 27;
 constexpr unsigned int minusPIN  = 22;
 
-int  eventRoutine(const ::gpiod::line_event& event) {
+int  eventRoutine(const gpiod::line_event& event) {
   int toRet=0;
-  if (event.event_type == ::gpiod::line_event::RISING_EDGE) {
-    std::cout << "\nRISING EDGE";
-  } else if (event.event_type == ::gpiod::line_event::FALLING_EDGE) {
-    std::cout << "\nFALLING EDGE";
+  if (event.event_type == gpiod::line_event::RISING_EDGE) {
+    std::cout << "RISING EDGE ";
+  } else if (event.event_type == gpiod::line_event::FALLING_EDGE) {
+    std::cout << "FALLING EDGE";
   } else {
-    throw ::std::logic_error("invalid event type");
+    throw std::logic_error("invalid event type");
   }
 
   
@@ -26,8 +28,6 @@ int  eventRoutine(const ::gpiod::line_event& event) {
   } else {
     toRet=-10;
   }
-
-  std::cout << ::std::endl;
   return toRet;
 }
 
@@ -35,22 +35,21 @@ int main (int, char**argv) {
   int retval =0;
   try{
     gpiod::chip GPIO("pinctrl-bcm2835");
-    FT1060M::PCF8591 i2c();
+    FT1060M::PCF8591 i2c;
     i2c.setAnalogOutputEnabled(true);
-    auto switch = GPIO.get_line(swithcPIN);
+    gpiod::line Switch = GPIO.get_line(switchPIN);
     bool wait = true;
-    if (!switch.is_requested()){      
-      switch.request({
+    if (!Switch.is_requested()){      
+      Switch.request({
 		      argv[0],
 		      ::gpiod::line_request::DIRECTION_INPUT,
 		      0
 	});
     } else {
-      //security
-      wait = false;
+      throw "Cannot request the switch";
     }
-
-    auto buttons = GPIO.get_lines({plusPIN,swithcPIN});
+    const int initialSwitchPosition = Switch.get_value();
+    gpiod::line_bulk buttons = GPIO.get_lines({plusPIN,minusPIN});
     buttons.request({
 		     argv[0],
 		     //gpiod::line_request::EVENT_BOTH_EDGES,
@@ -58,23 +57,22 @@ int main (int, char**argv) {
 		     0
       });
     int DACVAL=0;
-    while (wait) {
-      auto event = button.event_wait(::std::chrono::seconds(1));
-      if (event) {
-	DACVAL+=eventRoutine();
+    while (Switch.get_value()!=initialSwitchPosition) {
+      auto events = buttons.event_wait(::std::chrono::seconds(1));
+      if (events) {      
+	for (auto& lineWithEvent: events) {
+	  DACVAL+=eventRoutine(lineWithEvent.event_read());
+	}
 	if (DACVAL>255) {
 	  DACVAL=255;
 	} else if (DACVAL<0) {
 	  DACVAL=0;
 	}
 	i2c.writeToAnalogOut(DACVAL);
-      }
-      if(switch.get_value()==0) {
-	wait = false;
-      }
-    
+	std::cout << " : " << DACVAL << std::endl;
+      }   
     }
-  
+    std ::cout  << Switch.get_value()<< " "<< initialSwitchPosition <<std::endl;
   } catch (const char *problem) {
     std::cerr << "\033[1;31mERROR: \033[0m\033[1m" << problem << "\033[0m" << std::endl;
     retval = 1;
